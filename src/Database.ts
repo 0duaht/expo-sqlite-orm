a97cb6081f51e014b6bf4e816778459a29c84edb
Tobi Oduah <oduaht@gmail.com>
2023-07-08 17:54:26 -0700

    support expo 11.0.0+

--- src/Database.ts
+++ src/Database.ts
@@ -1,27 +1,30 @@
-import * as SQLite from 'expo-sqlite'
-import { SQLTransactionCallback, SQLTransactionErrorCallback, WebSQLDatabase } from 'expo-sqlite'
+import * as SQLite from "expo-sqlite";
+import {
+  SQLTransactionCallback,
+  SQLTransactionErrorCallback,
+} from "expo-sqlite";
 
 export class Database {
-  private databaseName: string
-  private database: WebSQLDatabase
-  private static instances: Record<string, Database> = {}
+  private databaseName: string;
+  private database: any;
+  private static instances: Record<string, Database> = {};
 
   private constructor(databaseName: string) {
-    this.databaseName = databaseName
+    this.databaseName = databaseName;
   }
 
   private openDatabase() {
     if (!this.database) {
-      this.database = SQLite.openDatabase(this.databaseName)
+      this.database = SQLite.openDatabase(this.databaseName);
     }
   }
 
   static instance(databaseName: string): Database {
     if (!this.instances[databaseName]) {
-      this.instances[databaseName] = new Database(databaseName)
+      this.instances[databaseName] = new Database(databaseName);
     }
 
-    return this.instances[databaseName]
+    return this.instances[databaseName];
   }
 
   transaction(
@@ -29,20 +32,20 @@ export class Database {
     errorCallback?: SQLTransactionErrorCallback,
     successCallback?: () => void
   ) {
-    this.openDatabase()
-    return this.database.transaction(callback, errorCallback, successCallback)
+    this.openDatabase();
+    return this.database.transaction(callback, errorCallback, successCallback);
   }
 
   async close(): Promise<void> {
-    if(!this.database) return
-    await this.database.closeAsync()
-    this.database = undefined
+    if (!this.database) return;
+    await this.database.closeAsync();
+    this.database = undefined;
   }
 
   async reset(): Promise<void> {
-    this.openDatabase()
-    await this.database.closeAsync()
-    await this.database.deleteAsync()
-    this.database = undefined
+    this.openDatabase();
+    await this.database.closeAsync();
+    await this.database.deleteAsync();
+    this.database = undefined;
   }
 }
